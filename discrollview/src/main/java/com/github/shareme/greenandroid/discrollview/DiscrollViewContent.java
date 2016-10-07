d0a0ae93a2a52004a1efb5d84f3aaaeba58d6fcd
Fred Grott <fred.grott@gmail.com>
2016-10-07 17:49:32 -0500

    added dsicrollview module and code

---
+++ discrollview/src/main/java/com/github/shareme/greenandroid/discrollview/DiscrollViewContent.java
@@ -0,0 +1,126 @@
+/*
+ Copyright 2013 Flavien Laurent
+ Modifications Copyright(C) 2016 Fred Grott(GrottWorkShop)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+package com.github.shareme.greenandroid.discrollview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+/**
+ *
+ */
+public class DiscrollViewContent extends LinearLayout {
+
+    public DiscrollViewContent(Context context) {
+        super(context);
+        setOrientation(VERTICAL);
+    }
+
+    public DiscrollViewContent(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setOrientation(VERTICAL);
+    }
+
+    public DiscrollViewContent(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setOrientation(VERTICAL);
+    }
+
+    @Override
+    public void addView(View child, int index, ViewGroup.LayoutParams params) {
+        super.addView(asDiscrollvable(child, (LayoutParams) params), index, params);
+    }
+
+    private View asDiscrollvable(View child, LayoutParams lp) {
+        if(! isDiscrollvable(lp)) {
+            return child;
+        }
+        DiscrollvableView discrollvableChild = new DiscrollvableView(getContext());
+        discrollvableChild.setDiscrollveAlpha(lp.mDiscrollveAlpha);
+        discrollvableChild.setDiscrollveTranslation(lp.mDiscrollveTranslation);
+        discrollvableChild.setDiscrollveScaleX(lp.mDiscrollveScaleX);
+        discrollvableChild.setDiscrollveScaleY(lp.mDiscrollveScaleY);
+        discrollvableChild.setDiscrollveThreshold(lp.mDiscrollveThreshold);
+        discrollvableChild.setDiscrollveFromBgColor(lp.mDiscrollveFromBgColor);
+        discrollvableChild.setDiscrollveToBgColor(lp.mDiscrollveToBgColor);
+        discrollvableChild.addView(child);
+        return discrollvableChild;
+    }
+
+    private boolean isDiscrollvable(LayoutParams lp) {
+        return lp.mDiscrollveAlpha ||
+               lp.mDiscrollveTranslation != -1 ||
+               lp.mDiscrollveScaleX ||
+               lp.mDiscrollveScaleY ||
+               (lp.mDiscrollveFromBgColor != -1 && lp.mDiscrollveToBgColor != -1);
+    }
+
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
+        return p instanceof LayoutParams;
+    }
+
+    @Override
+    protected LinearLayout.LayoutParams generateDefaultLayoutParams() {
+        return new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    public LinearLayout.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected LinearLayout.LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new LayoutParams(p.width, p.height);
+    }
+
+    public static class LayoutParams extends LinearLayout.LayoutParams {
+
+        private int mDiscrollveFromBgColor;
+        private int mDiscrollveToBgColor;
+        private float mDiscrollveThreshold;
+        public boolean mDiscrollveAlpha;
+        public boolean mDiscrollveScaleX;
+        public boolean mDiscrollveScaleY;
+        private int mDiscrollveTranslation;
+
+        public LayoutParams(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DiscrollView_LayoutParams);
+            try {
+                mDiscrollveAlpha = a.getBoolean(R.styleable.DiscrollView_LayoutParams_discrollve_alpha, false);
+                mDiscrollveScaleX = a.getBoolean(R.styleable.DiscrollView_LayoutParams_discrollve_scaleX, false);
+                mDiscrollveScaleY = a.getBoolean(R.styleable.DiscrollView_LayoutParams_discrollve_scaleY, false);
+                mDiscrollveTranslation = a.getInt(R.styleable.DiscrollView_LayoutParams_discrollve_translation, -1);
+                mDiscrollveThreshold = a.getFloat(R.styleable.DiscrollView_LayoutParams_discrollve_threshold, 0.0f);
+                mDiscrollveFromBgColor = a.getColor(R.styleable.DiscrollView_LayoutParams_discrollve_fromBgColor, -1);
+                mDiscrollveToBgColor = a.getColor(R.styleable.DiscrollView_LayoutParams_discrollve_toBgColor, -1);
+            } finally {
+                a.recycle();
+            }
+        }
+
+        public LayoutParams(int w, int h) {
+            super(w, h);
+        }
+    }
+}
---
+++ discrollview/src/main/java/com/github/shareme/greenandroid/discrollview/Discrollvable.java
@@ -0,0 +1,39 @@
+/*
+ Copyright 2013 Flavien Laurent
+ Modifications Copyright(C) 2016 Fred Grott(GrottWorkShop)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+ */
+package com.github.shareme.greenandroid.discrollview;
+
+/**
+ * Represents a View able to be discrollved.
+ */
+@SuppressWarnings("unused")
+public interface Discrollvable {
+
+    /**
+     * Called to discrollve the View.
+     * @param ratio discrollve ratio between 0.0 and 1.0.
+     *              1.0 means the View is totally discrollved
+     */
+    public void onDiscrollve(float ratio);
+
+    /**
+     * Called to reset the discrollvation of the View.
+     * In this method, you have to reset the View in order
+     * to be discrollved again.
+     */
+    public void onResetDiscrollve();
+}
