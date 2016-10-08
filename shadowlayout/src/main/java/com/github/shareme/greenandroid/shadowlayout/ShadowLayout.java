b15b761bc7a27de78d928806df7df9b41a7399ab
Fred Grott <fred.grott@gmail.com>
2016-10-08 07:15:05 -0500

    added shadowlayout moduel and code

---
+++ shadowlayout/src/main/java/com/github/shareme/greenandroid/shadowlayout/ShadowLayout.java
@@ -0,0 +1,153 @@
+/*
+ The MIT License (MIT)
+
+Copyright (c) 2014 Danylyk Dmytro
+Modifications Copyright(C) 2016 Fred Grott(GrottWorkShop)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.github.shareme.greenandroid.shadowlayout;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class ShadowLayout extends FrameLayout {
+
+    private int mFillColor;
+    private int mShadowColor;
+    private float mShadowRadius;
+    private float mCornerRadius;
+    private float mDx;
+    private float mDy;
+
+    public ShadowLayout(Context context) {
+        super(context);
+        initView(context, null);
+    }
+
+    public ShadowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context, attrs);
+    }
+
+    public ShadowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context, attrs);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if(w > 0 && h > 0) {
+            setBackgroundCompat(w, h);
+        }
+    }
+
+    private void initView(Context context, AttributeSet attrs) {
+        initAttributes(context, attrs);
+
+        int xPadding = (int) (mShadowRadius + Math.abs(mDx));
+        int yPadding = (int) (mShadowRadius + Math.abs(mDy));
+        setPadding(xPadding, yPadding, xPadding, yPadding);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void setBackgroundCompat(int w, int h) {
+        Bitmap bitmap = createShadowBitmap(w, h, mCornerRadius, mShadowRadius, mDx, mDy, mShadowColor, mFillColor);
+        BitmapDrawable drawable = new BitmapDrawable(getResources(), bitmap);
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN) {
+            setBackgroundDrawable(drawable);
+        } else {
+            setBackground(drawable);
+        }
+    }
+
+
+    private void initAttributes(Context context, AttributeSet attrs) {
+        TypedArray attr = getTypedArray(context, attrs, R.styleable.ShadowLayout);
+        if (attr == null) {
+            return;
+        }
+
+        try {
+            mCornerRadius = attr.getDimension(R.styleable.ShadowLayout_sl_cornerRadius, getResources().getDimension(R.dimen.default_corner_radius));
+            mShadowRadius = attr.getDimension(R.styleable.ShadowLayout_sl_shadowRadius, getResources().getDimension(R.dimen.default_shadow_radius));
+            mDx = attr.getDimension(R.styleable.ShadowLayout_sl_dx, 0);
+            mDy = attr.getDimension(R.styleable.ShadowLayout_sl_dy, 0);
+            mShadowColor = attr.getColor(R.styleable.ShadowLayout_sl_shadowColor, getResources().getColor(R.color.default_shadow_color));
+            mFillColor = attr.getColor(R.styleable.ShadowLayout_sl_fillColor, getResources().getColor(R.color.default_fill_color));
+        } finally {
+            attr.recycle();
+        }
+    }
+
+    private TypedArray getTypedArray(Context context, AttributeSet attributeSet, int[] attr) {
+        return context.obtainStyledAttributes(attributeSet, attr, 0, 0);
+    }
+
+    private Bitmap createShadowBitmap(int shadowWidth, int shadowHeight, float cornerRadius, float shadowRadius,
+                                      float dx, float dy, int shadowColor, int fillColor) {
+
+        Bitmap output = Bitmap.createBitmap(shadowWidth, shadowHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        RectF shadowRect = new RectF(
+                shadowRadius,
+                shadowRadius,
+                shadowWidth - shadowRadius,
+                shadowHeight - shadowRadius);
+
+        if (dy > 0) {
+            shadowRect.top += dy;
+            shadowRect.bottom -= dy;
+        } else if (dy < 0) {
+            shadowRect.top += Math.abs(dy);
+            shadowRect.bottom -= Math.abs(dy);
+        }
+
+        if (dx > 0) {
+            shadowRect.left += dx;
+            shadowRect.right -= dx;
+        } else if (dx < 0) {
+            shadowRect.left += Math.abs(dx);
+            shadowRect.right -= Math.abs(dx);
+        }
+
+        Paint shadowPaint = new Paint();
+        shadowPaint.setAntiAlias(true);
+        shadowPaint.setColor(fillColor);
+        shadowPaint.setStyle(Paint.Style.FILL);
+
+        if (!isInEditMode()) {
+            shadowPaint.setShadowLayer(shadowRadius, dx, dy, shadowColor);
+        }
+
+        canvas.drawRoundRect(shadowRect, cornerRadius, cornerRadius, shadowPaint);
+
+        return output;
+    }
+}
