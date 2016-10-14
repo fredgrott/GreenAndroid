fc48c6039dfaf0265fbe936dca227992b4ce356f
Fred Grott <fred.grott@gmail.com>
2016-10-14 05:54:14 -0500

    added slidebutton code

---
+++ slidebutton/src/main/java/com/github/shareme/greenandroid/slidebutton/Utils.java
@@ -0,0 +1,35 @@
+/*
+ Copyright 2016 Shitij
+ Modifications Copyright(C) 2016 Fred Grott(GrottWorkShop)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+package com.github.shareme.greenandroid.slidebutton;
+
+import android.content.res.Resources;
+
+/**
+ * Created by bruce on 14-11-6.
+ */
+public class Utils {
+    public static float dp2px(Resources resources, float dp) {
+        final float scale = resources.getDisplayMetrics().density;
+        return  dp * scale + 0.5f;
+    }
+
+    public static float sp2px(Resources resources, float sp){
+        final float scale = resources.getDisplayMetrics().scaledDensity;
+        return sp * scale;
+    }
+}
