4b2000e0372283181d87eab13fd837f35b9a643f
Fred Grott <fred.grott@gmail.com>
2016-10-16 10:18:23 -0500

    Materialize correction immersive flag cali is api 19 not api 16

--- materialize/src/main/java/com/github/shareme/greenandroid/materialize/MaterializeBuilder.java
+++ materialize/src/main/java/com/github/shareme/greenandroid/materialize/MaterializeBuilder.java
@@ -441,14 +441,23 @@ public class MaterializeBuilder {
 
         //set the flags if we want to hide the system ui
         if (mSystemUIHidden) {
+          //View.System_UI_FLAG_IMMERSIVE_STICKY is an api 19 call
             if (Build.VERSION.SDK_INT >= 16) {
                 View decorView = this.mActivity.getWindow().getDecorView();
                 decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+            }
+            if(Build.VERSION.SDK_INT >= 19){
+              View decorView = this.mActivity.getWindow().getDecorView();
+              decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                      | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                      | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                      | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                      | View.SYSTEM_UI_FLAG_FULLSCREEN
+                      | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             }
         }
 
