5186c92d7b408efab02be379fae3f80e75f5b12e
Fred Grott <fred.grott@gmail.com>
2016-10-20 07:53:43 -0500

    added funkyheadersapp code

---
+++ funkyheadersapp/src/main/java/com/github/shareme/greenandroid/funkyheadersapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.github.shareme.greenandroid.funkyheadersapp;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+        setContentView(R.layout.activity_main);
+    }
+}
