270c9a7120f4480adff0c1c8084c3e3480082003
Fred Grott <fred.grott@gmail.com>
2016-10-21 16:53:55 -0500

    added draglinearlayoutapp code

---
+++ draglinearlayoutapp/src/main/java/com/github/shareme/greenandroid/draglinearlayoutapp/DemoActivity.java
@@ -0,0 +1,32 @@
+package com.github.shareme.greenandroid.draglinearlayoutapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.github.shareme.greenandroid.draglinearlayout.DragLinearLayout;
+
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        DragLinearLayout dragLinearLayout = (DragLinearLayout) findViewById(R.id.container);
+        // set all children draggable except the first (the header)
+        for(int i = 1; i < dragLinearLayout.getChildCount(); i++){
+            View child = dragLinearLayout.getChildAt(i);
+            dragLinearLayout.setViewDraggable(child, child); // the child is its own drag handle
+        }
+
+        findViewById(R.id.noteDemoButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DemoActivity.this, NoteActivity.class));
+            }
+        });
+    }
+}
