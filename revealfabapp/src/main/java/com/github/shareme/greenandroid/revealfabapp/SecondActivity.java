77dfd6c417f7a133590dca77dbe37a510d437194
Fred Grott <fred.grott@gmail.com>
2016-10-21 10:04:51 -0500

    added revealfabapp code

---
+++ revealfabapp/src/main/java/com/github/shareme/greenandroid/revealfabapp/SecondActivity.java
@@ -0,0 +1,9 @@
+package com.github.shareme.greenandroid.revealfabapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sergiocasero on 15/4/16.
+ */
+public class SecondActivity extends AppCompatActivity {
+}
