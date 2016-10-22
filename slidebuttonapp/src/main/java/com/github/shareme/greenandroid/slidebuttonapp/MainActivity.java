154ad369e88699e5429b5b3971ac43fb8b8c6261
Fred Grott <fred.grott@gmail.com>
2016-10-21 19:14:00 -0500

    added slidebuttonapp code

---
+++ slidebuttonapp/src/main/java/com/github/shareme/greenandroid/slidebuttonapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.github.shareme.greenandroid.slidebuttonapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.github.shareme.greenandroid.slidebutton.SwipeButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SwipeButton swipeButton = (SwipeButton)findViewById(R.id.slide);
+        swipeButton.addOnSwipeCallback(new SwipeButton.Swipe() {
+            @Override
+            public void onButtonPress() {
+                Toast.makeText(MainActivity.this, "Pressed!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeCancel() {
+
+            }
+
+            @Override
+            public void onSwipeConfirm() {
+                Toast.makeText(MainActivity.this, "confirmd!", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
