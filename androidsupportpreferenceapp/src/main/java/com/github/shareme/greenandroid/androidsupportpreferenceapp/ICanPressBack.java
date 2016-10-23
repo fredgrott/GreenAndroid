1e183ce03b4d267e69717cab52cdaf5938cd514c
Fred Grott <fred.grott@gmail.com>
2016-10-23 14:54:41 -0500

    added androidsupportpreferenceapp code

---
+++ androidsupportpreferenceapp/src/main/java/com/github/shareme/greenandroid/androidsupportpreferenceapp/ICanPressBack.java
@@ -0,0 +1,8 @@
+package com.github.shareme.greenandroid.androidsupportpreferenceapp;
+
+/**
+ * Created by Eugen on 07.12.2015.
+ */
+public interface ICanPressBack {
+    boolean onBackPressed();
+}
